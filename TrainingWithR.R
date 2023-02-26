abd12244605298a56042076b160b099b79fbce19
Dona <donabl@gmail.com>
2023-02-26 09:34:55 -0600

    initial commit

--- TrainingWithR.R
+++ TrainingWithR.R
@@ -26,3 +26,29 @@ Tondu_labs <-as.numeric(TEDS_2016$Tondu,labels=c("Unification now”, “Status 
 Tondu_labs
 
 TEDS_2016 |> count(Tondu) |> ggplot(aes(x=Tondu,y=n)) + geom_point()
+
+
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(
+    linenumber = row_number(),
+    chapter = cumsum(str_detect(text, 
+                                regex("^chapter [\\divxlc]", 
+                                      ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+
+## Combine the lm, plot and abline functions to create a one step regression fit plot function
+regplot=function(x,y){
+  fit=lm(y~x)
+  plot(x,y)
+  abline(fit,col="red")
+}
+attach(TEDS_2016)
+regplot(Price,Sales)
+
